995489b62c1b0466b5d5db3c1250b9cd7f204f94
Christerresin <christer.resin@gmail.com>
2022-04-22 14:20:33 +0200

    add security for storage of member password

--- pages/api/members.js
+++ pages/api/members.js
@@ -1,6 +1,7 @@
 import dbConnect from '../../lib/dbConnect';
 import Member from '../../models/Member';
 import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
 
 export default async function handler(req, res) {
   const { method } = req;
@@ -19,12 +20,18 @@ export default async function handler(req, res) {
       }
       break;
     case 'POST':
+      const saltRounds = 10;
+
+      // check password
+      // let isCorrect = bcrypt.compareSync(req.body.password, hashedPassword);
+
       try {
+        /* create a new model in the database */
         const member = await Member.create({
           userId: uuidv4(),
           username: req.body.username,
-          password: req.body.password,
-        }); /* create a new model in the database */
+          password: bcrypt.hashSync(req.body.password, saltRounds),
+        });
         res.status(201).json({ success: true, data: member });
       } catch (error) {
         res.status(400).json({ success: false });
