0e1d922e3ee329db66d9492d98d4b3e46514d7ea
Christerresin <christer.resin@gmail.com>
2022-04-22 15:04:02 +0200

    add func to get a member and logic for registration to avoid duplicate usernames

--- pages/api/members.js
+++ pages/api/members.js
@@ -6,18 +6,20 @@ import bcrypt from 'bcrypt';
 export default async function handler(req, res) {
   const { method } = req;
 
+  console.log(req.query);
+
   await dbConnect();
 
   switch (method) {
     case 'GET':
       try {
-        const members = await Member.find(
-          {}
-        ); /* find all the data in our database */
+        /* find all the data in our database */
+        const members = await Member.find({});
         res.status(200).json({ success: true, data: members });
       } catch (error) {
         res.status(400).json({ success: false });
       }
+
       break;
     case 'POST':
       const saltRounds = 10;
@@ -25,20 +27,34 @@ export default async function handler(req, res) {
       // check password
       // let isCorrect = bcrypt.compareSync(req.body.password, hashedPassword);
 
-      try {
-        /* create a new model in the database */
-        const member = await Member.create({
-          userId: uuidv4(),
-          username: req.body.username,
-          password: bcrypt.hashSync(req.body.password, saltRounds),
-        });
-        res.status(201).json({ success: true, data: member });
-      } catch (error) {
-        res.status(400).json({ success: false });
+      let usernameAvailable = await getMember(req.body.username);
+
+      if (usernameAvailable.length === 0) {
+        try {
+          /* create a new model in the database */
+          const member = await Member.create({
+            userId: uuidv4(),
+            username: req.body.username,
+            password: bcrypt.hashSync(req.body.password, saltRounds),
+          });
+          res.status(201).json({ success: true, data: member });
+        } catch (error) {
+          res.status(400).json({ success: false });
+        }
+      } else {
+        res
+          .status(400)
+          .json({ success: false, message: 'Username unavailable' });
       }
+
       break;
     default:
       res.status(400).json({ success: false });
       break;
   }
 }
+
+const getMember = async (username) => {
+  const member = await Member.find({ username: username });
+  return member;
+};
