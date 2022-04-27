626f72ed6c4a18c61413336042062f372353828c
Christerresin <christer.resin@gmail.com>
2022-04-27 12:46:17 +0200

    replace switch case with ifs

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -7,86 +7,73 @@ import dbConnect from '../../../lib/dbConnect';
 import Member from '../../../models/Member';
 
 export default async function handler(req, res) {
-  const { method } = req;
-
   await dbConnect();
 
-  switch (method) {
-    case 'GET':
+  if (req.body === 'GET') {
+    try {
+      /* find all the data in our database */
+      const members = await Member.find({});
+      res.status(200).json({ success: true, data: members });
+    } catch (error) {
+      res.status(400).json({ success: false });
+    }
+  }
+
+  if (req.method === 'POST') {
+    const saltRounds = 10;
+    const isMember = await getMember(req.body.username);
+    const newMember = req.body.register;
+
+    if (isMember.length === 0 && newMember) {
       try {
-        /* find all the data in our database */
-        const members = await Member.find({});
-        res.status(200).json({ success: true, data: members });
+        /* create a new model in the database */
+        const member = await Member.create({
+          userId: uuidv4(),
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          username: req.body.username,
+          password: bcrypt.hashSync(req.body.password, saltRounds),
+          email: req.body.email,
+        });
+        res.status(201).json({ success: true, data: member });
       } catch (error) {
-        res.status(400).json({ success: false });
+        res
+          .status(400)
+          .json({ success: false, message: 'Something went wrong' });
       }
-      break;
+    }
 
-    case 'POST':
-      const saltRounds = 10;
-      const isMember = await getMember(req.body.username);
-      const newMember = req.body.register;
+    if (req.body.login === true) {
+      // if username is registered and entered password is correct, try to log the user in
+      const logingIn = bcrypt.compareSync(
+        req.body.password,
+        isMember[0].password
+      );
 
-      if (isMember.length === 0 && newMember) {
+      if (logingIn) {
         try {
-          /* create a new model in the database */
-          const member = await Member.create({
-            userId: uuidv4(),
-            firstname: req.body.firstname,
-            lastname: req.body.lastname,
-            username: req.body.username,
-            password: bcrypt.hashSync(req.body.password, saltRounds),
-            email: req.body.email,
-          });
-          res.status(201).json({ success: true, data: member });
-          break;
+          const cookies = new Cookies(req, res);
+          cookies.set(
+            'session',
+            await Iron.seal(
+              {
+                username: isMember[0].username,
+                loggedin: true,
+              },
+              process.env.ENC_KEY,
+              Iron.defaults
+            )
+          );
+          res.status(200).json({ success: true, message: 'Login route' });
         } catch (error) {
-          res
-            .status(400)
-            .json({ success: false, message: 'Something went wrong' });
-        }
-      }
-
-      if (req.body.login === true) {
-        // if username is registered and entered password is correct, try to log the user in
-        const logingIn = bcrypt.compareSync(
-          req.body.password,
-          isMember[0].password
-        );
-
-        if (logingIn) {
-          try {
-            const cookies = new Cookies(req, res);
-            cookies.set(
-              'session',
-              await Iron.seal(
-                {
-                  username: isMember[0].username,
-                  loggedin: true,
-                },
-                process.env.ENC_KEY,
-                Iron.defaults
-              )
-            );
-            res.status(200).json({ success: true, message: 'Login route' });
-          } catch (error) {
-            res.status(400).json({ success: false });
-          }
-        } else {
-          res
-            .status(400)
-            .json({ success: false, message: 'Username unavailable' });
+          res.status(400).json({ success: false });
         }
       } else {
         res
           .status(400)
           .json({ success: false, message: 'Username unavailable' });
       }
-      break;
-
-    default:
-      res.status(400).json({ success: false });
-      break;
+    }
   }
 }
 
